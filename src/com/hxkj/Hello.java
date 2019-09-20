f96c7c1dcf854d06df84790ff69cf53c22efbbdb
rh <1374761509@qq.com>
2019-09-20 12:21:31 +0800

    你好 第一次

---
+++ src/com/hxkj/Hello.java
@@ -0,0 +1,12 @@
+package com.hxkj;
+
+/**
+ * @Auther: 任 华
+ * @Date: 19-9-20 12:16
+ * @Description:
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
