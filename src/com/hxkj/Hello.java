a3a7986b3b762c9fea5f7eec85bb22b66d9f1211
rh <1374761509@qq.com>
2019-09-20 13:12:29 +0800

    第二次修改

--- src/com/hxkj/Hello.java
+++ src/com/hxkj/Hello.java
@@ -7,6 +7,6 @@ package com.hxkj;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello git!！");
+        System.out.println("hello git!！!!");
     }
 }
